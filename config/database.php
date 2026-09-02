9c75a338de7bc0ffa742609c3f295ea846634ae4
setiawanchan <setiawanchan28@gmail.com>
2026-09-02 15:11:09 +0700

    Force TCP/IP port 3306 in PDO DSN to prevent Unix socket 2002 error

--- config/database.php
+++ config/database.php
@@ -11,7 +11,8 @@ class Database {
 
     private function __construct() {
         try {
-            $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET;
+            $port = defined('DB_PORT') ? DB_PORT : '3306';
+            $dsn = "mysql:host=" . DB_HOST . ";port=" . $port . ";dbname=" . DB_NAME . ";charset=" . DB_CHARSET;
             $options = [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
